59e26d83fc716556361aba1d15272ce5a22c1ccb
Niclas <niclas.victorsson@medieinstitutet.se>
2018-11-10 20:29:53 +0100

    added portfoliosection for digitalAds

--- js/portfolio.js
+++ js/portfolio.js
@@ -11,7 +11,7 @@ class frontendItem {
     }
 }
 
-class designItem {
+class webDesignItem {
     constructor(title, description, group, image) {
         this.title = title;
         this.description = description;
@@ -25,6 +25,24 @@ class photo {
         this.url = url;
         this.title = text;
         this.group = "Photo";
+    }
+}
+
+class digitalAds {
+    constructor(title, desc, image) {
+        this.category = "Digital ad",
+        this.desc = desc
+        this.title = title, 
+        this.image = image,
+        this.ads = []
+    }
+}
+class digitalAdItem {
+    constructor(type, desc, url, flash) {
+        this.type = type,
+        this.url = url,
+        this.flash = flash,
+        this.desc = desc
     }
 }
 
@@ -65,17 +83,116 @@ createModule = {
     designWorks: () => {
         let portfolio = JSON.parse(sessionStorage.getItem('portfolio'));
 
-        let besafer = new designItem("BeSafer", "Design av wordpress webb", "Webdesign", "images/portfolio/design/besafer.jpg");
-        let snaps = new designItem("Snaps Bar & Bistro", "Design av wordpress webb + fotografering", "Webdesign", "images/portfolio/design/snaps.jpg")
-        let gronastugan = new designItem("Gröna Stugan", "Design av wordpress webb + fotografering", "Webdesign", "images/portfolio/design/gronastugan.jpg")
-        let forexsson = new designItem("Forexsson", "Design av wordpress webb", "Webdesign", "images/portfolio/design/forexsson.jpg")
-        let vaznaz = new designItem("Vaznaz bygg", "Design av wordpress webb", "Webdesign", "images/portfolio/design/vaznaz.jpg")
-        let elixir = new designItem("Elixir Juicebar", "Design & Branding av webb", "Webdesign", "images/portfolio/design/elixir.jpg")
+        let besafer = new webDesignItem("BeSafer", "Design av wordpress webb", "Webdesign", "images/portfolio/design/besafer.jpg");
+        let snaps = new webDesignItem("Snaps Bar & Bistro", "Design av wordpress webb + fotografering", "Webdesign", "images/portfolio/design/snaps.jpg")
+        let gronastugan = new webDesignItem("Gröna Stugan", "Design av wordpress webb + fotografering", "Webdesign", "images/portfolio/design/gronastugan.jpg")
+        let forexsson = new webDesignItem("Forexsson", "Design av wordpress webb", "Webdesign", "images/portfolio/design/forexsson.jpg")
+        let vaznaz = new webDesignItem("Vaznaz bygg", "Design av wordpress webb", "Webdesign", "images/portfolio/design/vaznaz.jpg")
+        let elixir = new webDesignItem("Elixir Juicebar", "Design & Branding av webb", "Webdesign", "images/portfolio/design/elixir.jpg")
         portfolio.push(besafer, snaps, gronastugan, elixir, forexsson, vaznaz);
 
         sessionStorage.setItem("portfolio", JSON.stringify(portfolio));
 
 
+    },
+
+    digitalAds: function() {
+        let portfolio = JSON.parse(sessionStorage.getItem('portfolio'));
+
+        let batmanArkhamCity = new digitalAds("Batman Arkham City", "Formatanpassningar, framtagning av takeoverlösning", 
+        "https://upload.wikimedia.org/wikipedia/en/0/00/Batman_Arkham_City_Game_Cover.jpg");
+        let BACBanner = new digitalAdItem("Banner", "300x600", "http://obbyone.com/kampanjer/batman_arkham_city/banner", true);
+        let BACTakeover = new digitalAdItem("Takeover", "970x250, 300x250, Wallpaper", "http://obbyone.com/kampanjer/batman_arkham_city/takeover", true);
+        batmanArkhamCity.ads.push(BACBanner, BACTakeover);
+
+        let batmanArkhamOrigins = new digitalAds("Batman Arkham Origins", "Formatanpassningar, framtagning av takeoverlösning",
+        "https://www.komplett.se/img/p/800/6518253c-15b6-43b4-a15b-eb63c233ac37.jpg");
+        let BAOBanner = new digitalAdItem("Banner", "468x1000", "http://obbyone.com/kampanjer/batman_arkham_origins/banner", true)
+        let BAOTakeover = new digitalAdItem("Takeover", "1000x300, Wallpaper", "http://obbyone.com/kampanjer/batman_arkham_origins/takeover", true);
+        batmanArkhamOrigins.ads.push(BAOBanner, BAOTakeover);
+
+        let boardwalkEmpireS3 = new digitalAds("Boardwalk Empire s3", "Formatanpassningar", 
+        "https://images-na.ssl-images-amazon.com/images/I/91p92fa0sqL._SX342_.jpg");
+        let BEse = new digitalAdItem("Vanner - SE", "980x480", "http://obbyone.com/kampanjer/boardwalk_empire_s3/se", true)
+        let BEdk = new digitalAdItem("Banners - DK", "930x180, 300x250, 160x600", "http://obbyone.com/kampanjer/boardwalk_empire_s3/dk", true) 
+        boardwalkEmpireS3.ads.push(BEse, BEdk);
+
+        let cosmopolis = new digitalAds("Cosmopolis", "Framtagning original", 
+        "https://upload.wikimedia.org/wikipedia/en/thumb/a/ad/Cosmopolis_Poster.jpg/220px-Cosmopolis_Poster.jpg");
+        let cosmopolisBanner = new digitalAdItem("Banner", "300x250", "http://obbyone.com/kampanjer/cosmopolis", true )
+        cosmopolis.ads.push(cosmopolisBanner);
+
+        let crazyStupidLove = new digitalAds("Crazy Stupid Love", "Formatanpassning", 
+        "https://is1-ssl.mzstatic.com/image/thumb/Video18/v4/96/47/c5/9647c5a5-656d-52c1-85aa-fc2e6e83faf9/pr_source.lsr/1200x630bb.png");
+        let crazyStupidLoveBanner = new digitalAdItem("Banner", "180x500", "http://obbyone.com/kampanjer/crazy_stupid_love", true );
+        crazyStupidLove.ads.push(crazyStupidLoveBanner);
+
+        let finalDestination5 = new digitalAds("Final Destination 5", "Formatanpassning",
+        "https://m.media-amazon.com/images/M/MV5BMTgyOTExNDc1M15BMl5BanBnXkFtZTcwMDA0MTA4NQ@@._V1_.jpg");
+        let finalDestination5Banner = new digitalAdItem("Banners", "728x90, 468x400", "http://obbyone.com/kampanjer/final_destination_5/se", true );
+        finalDestination5.ads.push(finalDestination5Banner);
+
+        let helloGotland = new digitalAds("Hello Gotland", "Framtagning av original", 
+        "http://obbyone.com/kampanjer/hellogotland/hg_300x400.jpg");
+        let helloGotlandBanners = new digitalAdItem("JPG Banners", "300x300, 300x400, 700x250, 700x400", "http://obbyone.com/kampanjer/hellogotland/", false);
+        helloGotland.ads.push(helloGotlandBanners);
+
+        let imStillHere = new digitalAds("Im Still Here", "Formatanpassning",
+        "https://m.media-amazon.com/images/M/MV5BNTE5NjA2NzM5Ml5BMl5BanBnXkFtZTcwODg5NTE3Mw@@._V1_.jpg");
+        let imStillHereBanner = new digitalAdItem("Banner", "250x360", "http://obbyone.com/kampanjer/im_still_here/", true);
+        imStillHere.ads.push(imStillHereBanner);
+
+        let infiniteCrisis = new digitalAds("Infinite Crisis", "Formatanpassningar",
+        "https://media.moddb.com/images/games/1/25/24871/boxshot.jpg");
+        let infiniteCrisisBanner = new digitalAdItem("Banners", "628x300, 1100x300, 260x600", "http://obbyone.com/kampanjer/infinite_crisis/se/", true);
+        infiniteCrisis.ads.push(infiniteCrisisBanner);
+
+        let ironClad = new digitalAds("Ironclad", "Framtagning original",
+        "http://www.medievalists.net/wp-content/uploads/2009/08/ironclad-officialposter.jpg"); 
+        let ironCladBanner = new digitalAdItem("Banner", "728x90", "http://obbyone.com/kampanjer/ironclad/banner/", true);
+        let ironCladEannounce = new digitalAdItem("E-Announce", "E-announce", "http://obbyone.com/kampanjer/ironclad/eannounce/", false);
+        ironClad.ads.push(ironCladBanner, ironCladEannounce);
+
+        let killList = new digitalAds("Kill list", "Framtagning av original", 
+        "http://cinemadan.com/blog/wp-content/uploads/2012/03/kl.jpg");
+        let killListBanner = new digitalAdItem("Banner", "728x90", "http://obbyone.com/kampanjer/kill_list/banner/", true);
+        let killListEannounce = new digitalAdItem("E-Announce", "Eannounce", "http://obbyone.com/kampanjer/kill_list/eannounce", false);
+        killList.ads.push(killListBanner, killListEannounce);
+
+        let lollipopChainsaw = new digitalAds("Lollipop Chainsaw", "Formatanpassningar och framtagning av takeoverlösning",
+        "https://upload.wikimedia.org/wikipedia/en/3/39/Lollipop_Chainsaw_Cover_Art.png");
+        let LCFI = new digitalAdItem("Takeoverlösning - FI", "Banner + Wallpaper", "http://obbyone.com/kampanjer/lollipop_chainsaw/fi/cdon/", true);
+        let LCNO = new digitalAdItem("Takeoverlösning - NO", "Banner + Wallpaper", "http://obbyone.com/kampanjer/lollipop_chainsaw/no/pressfire/", true);
+        let LCtakeover = new digitalAdItem("Takeoverlösning - SE", "Banner + Wallpaper", "http://obbyone.com/kampanjer/lollipop_chainsaw/se/eurogamer/", true);
+        let LCBanner1 = new digitalAdItem("Banners 1", "728x90, 336x280", "http://obbyone.com/kampanjer/lollipop_chainsaw/se/google/", true);
+        let LCBanner2 = new digitalAdItem("Banners 2", "300x250, 160x600", "http://obbyone.com/kampanjer/lollipop_chainsaw/se/google2/", true);
+        lollipopChainsaw.ads.push(LCFI, LCNO, LCtakeover, LCBanner1, LCBanner2);
+
+        let lotro = new digitalAds("Lord of the rings: Online", "Takeoverlösning",
+        "http://drh1.img.digitalriver.com/DRHM/Storefront/Company/turbine/images/product/misc/lotro-ror-title-screen.jpg");
+        let lotroBanner = new digitalAdItem("Takeoverlösning", "Banner + Wallpaper", "http://obbyone.com/kampanjer/lotro/se/", true);
+        lotro.ads.push(lotroBanner);
+
+        let mixedTvseries = new digitalAds("Mixed Tv-series - NO", "Framtagning av original",
+        "http://www.bsmc.net.au/wp-content/uploads/No-image-available.jpg");
+        let mixedTvseriesBanner = new digitalAdItem("Banners", "728x90, 300x250, 160x600", "http://obbyone.com/kampanjer/mixed_tvseries/no/", true);
+        mixedTvseries.ads.push(mixedTvseriesBanner);
+
+        let mixedTvseries2 = new digitalAds("Mixed Tv-series 2 - NO", "Framtagning av original", 
+        "http://www.bsmc.net.au/wp-content/uploads/No-image-available.jpg");
+        let mixedTvseriesBanner2 = new digitalAdItem("Banners", "980x150, 468x400", "http://obbyone.com/kampanjer/tvserier/", true);
+        mixedTvseries2.ads.push(mixedTvseriesBanner2);
+
+        let tyrannosaur = new digitalAds("Tyrannosaur", "Framtagning av original",
+        "https://m.media-amazon.com/images/M/MV5BMjIwNzAxMDgxM15BMl5BanBnXkFtZTcwODM5NzU4NQ@@._V1_UX182_CR0,0,182,268_AL_.jpg" );
+        let tyrannosaurBanner = new digitalAdItem("Banner", "728x90", "http://obbyone.com/kampanjer/tyrannosaur/banner/", true);
+        let tyrannosaurEannounce = new digitalAdItem("E-announce", "Eannounce", "http://obbyone.com/kampanjer/tyrannosaur/eannounce", false);
+        tyrannosaur.ads.push(tyrannosaurBanner, tyrannosaurEannounce);
+
+        portfolio.push(lollipopChainsaw, batmanArkhamOrigins, batmanArkhamCity, boardwalkEmpireS3, cosmopolis, crazyStupidLove, finalDestination5, 
+        imStillHere, infiniteCrisis, ironClad, killList, tyrannosaur, helloGotland, lotro, mixedTvseries, mixedTvseries2);
+
+        sessionStorage.setItem("portfolio", JSON.stringify(portfolio));
     },
 
     photos: () => {
@@ -141,17 +258,31 @@ createModule = {
         }
     },
 
+    logPortfolio: () => {
+        let portfolio = JSON.parse(sessionStorage.getItem('portfolio'));
+        console.log(portfolio);
+    },
+
     addToSessionStorage: (portfolio) => {
         sessionStorage.setItem("portfolio", JSON.stringify(portfolio));
     },
+
+    checkFlash: function(title) {
+        if(title === true) {
+            return `<span class="badge badge-light" style="margin-left: 5px; font-size: 0.8em">FLASH-AD</span>`;
+        }
+    }, 
 
     init_: function() {
         this.frontendWorks();
         this.designWorks();
         this.photos();
+        this.digitalAds();
         displayModule.frontendWorks(); 
         displayModule.designWorks();
+        displayModule.digitalAds();
         displayModule.photogallery();
+        this.logPortfolio();
     }
 }
 
@@ -221,6 +352,44 @@ displayModule = {
         }
         frontendDiv.innerHTML = content;
         main.insertAdjacentElement('beforeend', frontendDiv)
+    },
+
+    digitalAds: function() {
+        let portfolio = JSON.parse(sessionStorage.getItem('portfolio'));
+        let main = document.getElementsByTagName('main')[0];
+        let content = `<h2>Digitala Annonser</h2>`;
+        let digitalAdsDiv = document.createElement('div');
+        digitalAdsDiv.classList.add('flexbox', 'portfolio-section');
+
+        for (let item of portfolio) {
+            if (item.category === "Digital ad") {
+                let ads = "";
+                for(let ad of item.ads) {
+                    let flash = ad.flash;
+                    ads += `<a href="${ad.url}" class="btn btn-outline-warning" target="_blank" 
+                    data-toggle="tooltip" data-placement="bottom" title="${ad.desc}" data-original-title="Tooltip on bottom">${ad.type} 
+                    ${createModule.checkFlash(flash)}</a>                    
+                    `
+                }
+                content += `
+                <div class="card border-warning mb-3 portfolio-item-4col center">
+                    <div class="card-header">
+                        <p class="header">${item.title}</p>
+                        <small class="form-text text-primary">${item.desc}</small>
+                    </div>
+                    <div class="card-body">
+                        <div class="webdesign-img-container" style="margin-bottom: 15px;">
+                        <div class="adImage" style="background-image: url('${item.image}');" class="img-fluid"></div>
+                        <div class="ads"> ${ads} </div>
+                        </div>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+                `;
+            }
+        }
+        digitalAdsDiv.innerHTML = content;
+        main.insertAdjacentElement('beforeend', digitalAdsDiv)
     },
 
     photogallery: () => {
